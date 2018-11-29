ed90b62aed15e7fa1540ccaeac8bf03048edd71c
sequ219 <12060219sequ@gmail.com>
2018-11-29 10:47:34 -0500

    I wrote the dict code

--- dictionary_test.py
+++ dictionary_test.py
@@ -10,13 +10,20 @@ import csv
 my_dir = os.path.dirname(os.path.realpath(__file__))
 filename = 'dictionary_test.csv'
 
-# TODO: read the data from the csv file and put into dictionary
+my_dict = dict()
 
+with open(my_dir+'/'+filename,'r')as f:
+
+    csv_reader=csv.reader(f)
+    for row in csv_reader:
+        key=row[0]
+        value=row[1]
+        my_dict[key] = value
 
 # print(read_data)
 
 
-print(my_dict)
+    print(my_dict)
 
 # This should print out 'Joe'
-print(my_dict[1])
+# print(my_dict[1])
